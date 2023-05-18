cf71c668fb5c81f4f8aff8a76702af238cf8820b
avedineev <avedineev69@gmail.com>
2023-05-18 22:40:17 +0500

    homework4 - исправил ,gitignore

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,41 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        """
+        self.data = data
+        self.next_node = None
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
-    def insert_beginning(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+    def __init__(self):
+
+        self.head = None
+        self.next_node = None
+
+    def insert_beginning(self, data: dict):
+        """Принимает; данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.next_node is not None:
+                n = n.next_node
+            n.next_node = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,41 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+
+from src.linked_list import LinkedList
+from src.queue import Node
+
+
+class TestNodeInit(unittest.TestCase):
+
+    def test__init__Node(self):
+        new_node = Node('data')
+
+        self.assertEqual(new_node.data, 'data')
+        self.assertEqual(new_node.next_node, None)
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def test__init__Node(self):
+        ll = LinkedList()
+        self.assertEqual(ll.head, None)
+        self.assertEqual(ll.next_node, None)
+
+    def test_insert_beginning(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        self.assertEqual(ll.head.data, {'id': 1})
+
+        ll.insert_beginning({'id': 0})
+        self.assertEqual(ll.head.data, {'id': 0})
+
+    def test_insert_at_end(self):
+        ll = LinkedList()
+
+        # Добавляем данные
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        self.assertEqual(ll.head.data, {'id': 1})
+        self.assertEqual(ll.head, {'id': 2})
+        ll.insert_at_end({'id': 3})
+
